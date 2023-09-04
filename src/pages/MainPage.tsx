235afb812448a9086c6801541c89baa90d7ea5ad
seon-mikim <tjsalsun@gmail.com>
2023-09-05 05:02:06 +0900

    refactor: 불필요한 모듈 제거

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,5 @@
-import getNewsApi from '@utils/api/getNewsApi'
-import React from 'react'
-
 const MainPage = () => {
-	getNewsApi({ keyword: 'apple', date: '2023-09-05' })
+
 	return (
 		<div>MainPage</div>
 	)
