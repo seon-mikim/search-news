8d657ba687d809b45bec5b3009bf2591e19b43a3
seon-mikim <tjsalsun@gmail.com>
2023-09-05 04:58:07 +0900

    feat: newApi 구현

--- src/utils/api/getNewsApi.ts
+++ src/utils/api/getNewsApi.ts
@@ -4,10 +4,9 @@ const getNewsApi = async ({ keyword, date }: NewsApi) => {
   try {
     const response = await fetch(
       `https://newsapi.org/v2/everything?q=${keyword}&from=${date}&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}`
-		);
-		console.log(response.json())
-		return response.json();
-		
+    );
+    const responseData = await response.json();
+    return responseData;
   } catch (err) {
     console.error(err);
   }
