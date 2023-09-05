bb21777c8313e8e5546197ba316acd3a51851ceb
seon-mikim <tjsalsun@gmail.com>
2023-09-05 09:47:16 +0900

    design: CardItem 컴포넌드 디자인 추가
    - title, description 디자인 추가

--- src/components/NewsCardItem/NewsCardItemTitle.tsx
+++ src/components/NewsCardItem/NewsCardItemTitle.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 interface NewsCardItemTitleProps {
   title: string;
+  className: string
 }
 
-const NewsCardItemTitle = ({ title }: NewsCardItemTitleProps) => {
-  return <div>{title}</div>;
+const NewsCardItemTitle = ({ title, className }: NewsCardItemTitleProps) => {
+  return <div className={className }>{title}</div>;
 };
 
 export default NewsCardItemTitle;
