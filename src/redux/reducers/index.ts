5131e5499716652e530f9290e067e45f3dd0499c
seon-mikim <tjsalsun@gmail.com>
2023-09-08 09:48:15 +0900

    feat: hook 생성

--- src/redux/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import newApiDataReducer from './newApiData';
-const rootReducer = combineReducers({
-	newsData: newApiDataReducer
-});
-
-export type RootState = ReturnType<typeof rootReducer>
-export default rootReducer
